3681b7243f161564a291aea50f56bd2901551b7d
Yvette Schröder <yvette@MacBook-Pro-von-Yvette.local>
2022-05-16 17:17:49 +0200

    Clearer look at local playlists and dinner, percentual values, fix mistakes

--- functional/__init__.py
+++ functional/__init__.py
@@ -1,1 +1,2 @@
-from .helpers import clean_df, get_data, process, unified, clean
+from .helpers import clean_df, get_data, process, add_counts, unified, clean, map_color
+from .findings import analyze_most_different
--- functional/helpers.py
+++ functional/helpers.py
@@ -3,14 +3,19 @@ import re
 
 def get_data() -> tuple[pd.DataFrame, pd.DataFrame]:
     '''Loads the data from the excel files'''
-    full_male = pd.read_excel('data/male_complete.xlsx')
-    full_female = pd.read_excel('data/female_complete.xlsx')
+    full_male = pd.read_csv('data/male_complete.csv')
+    full_female = pd.read_csv('data/female_complete.csv')
 
     return full_male, full_female
 
 def process(df: pd.DataFrame) -> pd.DataFrame:
     '''Returns a filtered version of the df containing only data we have for every row'''
     return df[['day', 'time', 'hub name','playlist name', 'type']]
+
+def add_counts(df: pd.DataFrame) -> pd.DataFrame:
+    df['playlist count'] = df.groupby('playlist name')['playlist name'].transform('count')
+    df['hub count'] = df.groupby('hub name')['hub name'].transform('count')
+    return df
 
 def unified(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
     return pd.concat([df1, df2])
@@ -27,5 +32,17 @@ def clean_df(df: pd.DataFrame) -> pd.DataFrame:
     '''Cleans texts for semantic comparison'''
     df['hub name'] = df['hub name'].apply(clean)
     df['playlist name'] = df['playlist name'].apply(clean)
+    df['time'] = df.time.astype(str).apply(lambda x: int(x[:2]))
 
+    for i, time in df.time.iteritems():
+        if time < 12:
+            df.iloc[i] = 'morning'
+        elif time >=12<19:
+            df.iloc[i] = 'afternoon'
+        else:
+            df.iloc[i] = 'evening'
     return df
+
+def map_color(x, all) -> list[str]:
+    '''Creates colorlist for male and female user's features'''
+    return ['blue' if e in x else 'red' for e in all]
